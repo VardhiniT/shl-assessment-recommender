bcc2b7e82e67ae1717b0654a88d82cda64bd6855
VardhiniT <vardhinitelu.25@gmail.com>
2026-05-17 19:06:28 +0530

    track vector db and fix retrieval

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -1,99 +1,11 @@
-import json
 import chromadb
 
-from rag.embeddings import embedding_model
+CHROMA_PATH="vector_db"
 
-
-# --------------------------------------------------
-# CHROMA CONFIG
-# --------------------------------------------------
-
-client = chromadb.Client()
-
-
-# --------------------------------------------------
-# VECTOR DB CREATION
-# --------------------------------------------------
-
-def create_vector_db(collection):
-
-    with open(
-        "data/processed_assessments.json",
-        "r"
-    ) as f:
-
-        data = json.load(f)
-
-    documents=[]
-    embeddings=[]
-    metadatas=[]
-    ids=[]
-
-    for idx,item in enumerate(data):
-
-        search_text=item["search_text"]
-
-        embedding=(
-            embedding_model
-            .encode(search_text)
-            .tolist()
-        )
-
-        documents.append(
-            search_text
-        )
-
-        embeddings.append(
-            embedding
-        )
-
-        metadatas.append({
-
-            "name":
-            item["name"],
-
-            "url":
-            item["url"],
-
-            "test_type":
-            item["test_type"],
-
-            "duration":
-            item["duration"],
-
-            "remote_testing":
-            item["remote_testing"]
-
-        })
-
-        ids.append(
-            str(idx)
-        )
-
-    collection.add(
-        documents=documents,
-        embeddings=embeddings,
-        metadatas=metadatas,
-        ids=ids
-    )
-
-    print(
-        "Vector DB created"
-    )
-
-
-# --------------------------------------------------
-# CREATE COLLECTION
-# --------------------------------------------------
-
-print(
-    "Creating collection"
+client = chromadb.PersistentClient(
+    path=CHROMA_PATH
 )
 
 collection = client.get_or_create_collection(
     name="shl_assessments"
-)
-
-create_vector_db(
-    collection
 )
